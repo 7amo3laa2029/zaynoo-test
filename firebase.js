44b496c6e42c003196d69b7faad53839e54b6a78
7amo3laa2029 <115052088+7amo3laa2029@users.noreply.github.com>
2026-01-22 16:42:04 +0200

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAqXr5fR-hnq2GVz6NQniMSxo1POur6XZc",
+  authDomain: "zaynoo-a176e.firebaseapp.com",
+  projectId: "zaynoo-a176e",
+  storageBucket: "zaynoo-a176e.firebasestorage.app",
+  messagingSenderId: "652257259407",
+  appId: "1:652257259407:web:bb81ce5ef6e31e8ffbe5fa"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
