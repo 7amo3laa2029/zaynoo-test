74b7d4a1279645343e2f1ed7ae60f838d7740cb2
7amo3laa2029 <115052088+7amo3laa2029@users.noreply.github.com>
2026-01-22 17:45:37 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyAqXr5fR-hnq2GVz6NQniMSxo1POur6XZc",
   authDomain: "zaynoo-a176e.firebaseapp.com",
   projectId: "zaynoo-a176e",
-  storageBucket: "zaynoo-a176e.firebasestorage.app",
+  storageBucket: "zaynoo-a176e.appspot.com",
   messagingSenderId: "652257259407",
   appId: "1:652257259407:web:bb81ce5ef6e31e8ffbe5fa"
 };
